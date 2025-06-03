b85c840073570b26b510fb66f02c3aee42d115d9
TawhidShakil <astalukdar39@gmail.com>
2025-06-03 20:42:19 +0600

    added my portfolio file

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import AboutMe from "./components/AboutMe"
+import Contact from "./components/Contact"
+import Footer from "./components/footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Service from "./components/Service"
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AboutMe />
+      <Service />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
