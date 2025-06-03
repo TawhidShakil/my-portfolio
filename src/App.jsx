9026f38ce672cf198804dedbdb9c4772c6082468
TawhidShakil <astalukdar39@gmail.com>
2025-06-03 21:11:27 +0600

    rename import file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AboutMe from "./components/AboutMe"
 import Contact from "./components/Contact"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
